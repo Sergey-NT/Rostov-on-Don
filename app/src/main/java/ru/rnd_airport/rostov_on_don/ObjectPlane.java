3290438a235ecd1dc7c77d68507dffa476fd9467
Sergey Uharskiy <uharskiy@gmail.com>
2016-10-21 12:00:59 +0500

    Initial commit

---
+++ app/src/main/java/ru/rnd_airport/rostov_on_don/ObjectPlane.java
@@ -0,0 +1,126 @@
+package ru.rnd_airport.rostov_on_don;
+
+public class ObjectPlane {
+
+    private String planeFlight;
+    private String planeDirection;
+    private String planeType;
+    private String planeTimePlan;
+    private String planeTimeFact;
+    private String planeStatus;
+    private String planeBaggageStatus;
+    private String planeGate;
+    private String planeCheckIn;
+    private String planeCombination;
+    private String planeRoute;
+    private String planeRouteStatus;
+    private String planeAirline;
+    private String registrationBegin;
+    private String registrationEnd;
+    private String checkInStatus;
+    private String boardingStatus;
+    private String boardingEnd;
+    private boolean planeTracking;
+
+    public ObjectPlane(String planeFlight, String planeDirection, String planeType, String planeTimePlan, String planeTimeFact, String planeStatus, boolean planeTracking, String planeBaggageStatus, String planeGate, String planeCheckIn, String planeCombination, String planeRoute, String planeRouteStatus, String registrationBegin, String registrationEnd, String checkInStatus, String boardingEnd, String boardingStatus, String planeAirline) {
+        this.planeFlight = planeFlight;
+        this.planeDirection = planeDirection;
+        this.planeType = planeType;
+        this.planeTimePlan = planeTimePlan;
+        this.planeTimeFact = planeTimeFact;
+        this.planeStatus = planeStatus;
+        this.planeTracking = planeTracking;
+        this.planeBaggageStatus = planeBaggageStatus;
+        this.planeGate = planeGate;
+        this.planeCheckIn = planeCheckIn;
+        this.planeCombination = planeCombination;
+        this.planeRoute = planeRoute;
+        this.planeRouteStatus = planeRouteStatus;
+        this.planeAirline = planeAirline;
+        this.registrationBegin = registrationBegin;
+        this.registrationEnd = registrationEnd;
+        this.checkInStatus = checkInStatus;
+        this.boardingEnd = boardingEnd;
+        this.boardingStatus = boardingStatus;
+    }
+
+    public String getPlaneFlight() {
+        return planeFlight;
+    }
+
+    public String getPlaneDirection() {
+        return planeDirection;
+    }
+
+    public String getPlaneType() {
+        return planeType;
+    }
+
+    public String getPlaneTimePlan() {
+        return planeTimePlan;
+    }
+
+    public String getPlaneTimeFact() {
+        return planeTimeFact;
+    }
+
+    public String getPlaneStatus() {
+        return planeStatus;
+    }
+
+    public String getPlaneBaggageStatus() {
+        return planeBaggageStatus;
+    }
+
+    public String getPlaneGate() {
+        return planeGate;
+    }
+
+    public String getPlaneCheckIn() {
+        return planeCheckIn;
+    }
+
+    public String getPlaneCombination() {
+        return planeCombination;
+    }
+
+    public String getPlaneRoute() {
+        return planeRoute;
+    }
+
+    public String getPlaneRouteStatus() {
+        return planeRouteStatus;
+    }
+
+    public String getPlaneAirline() {
+        return planeAirline;
+    }
+
+    public String getRegistrationBegin() {
+        return registrationBegin;
+    }
+
+    public String getRegistrationEnd() {
+        return registrationEnd;
+    }
+
+    public String getCheckInStatus() {
+        return checkInStatus;
+    }
+
+    public String getBoardingStatus() {
+        return boardingStatus;
+    }
+
+    public String getBoardingEnd() {
+        return boardingEnd;
+    }
+
+    public boolean isPlaneTracking() {
+        return planeTracking;
+    }
+
+    public void setPlaneTracking(boolean planeTracking) {
+        this.planeTracking = planeTracking;
+    }
+}
---
+++ app/src/main/java/ru/rnd_airport/rostov_on_don/gcm/AppFirebaseMessagingService.java
@@ -0,0 +1,70 @@
+package ru.rnd_airport.rostov_on_don.gcm;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import ru.rnd_airport.rostov_on_don.Constants;
+import ru.rnd_airport.rostov_on_don.MainActivity;
+import ru.rnd_airport.rostov_on_don.R;
+
+public class AppFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "FirebaseMessaging";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        int icon;
+
+        String message = remoteMessage.getData().get("message");
+        String planeNumber = remoteMessage.getData().get("title");
+        String direction = remoteMessage.getData().get("direction");
+        String title = getString(R.string.plane_desc_flight) + " " + planeNumber + " " + remoteMessage.getData().get("plane_direction");
+
+        if (Constants.LOG_ON) {
+            Log.d(TAG, "Message: " + message);
+            Log.d(TAG, "Title: " + title);
+            Log.d(TAG, "Plane number: " + planeNumber);
+            Log.d(TAG, "Direction: " + direction);
+        }
+
+        if (direction != null) {
+            if (direction.equals("arrival")) {
+                icon = R.mipmap.ic_flight_land_white_24dp;
+            } else {
+                icon = R.mipmap.ic_flight_takeoff_white_24dp;
+            }
+            sendNotification(title, message, icon, planeNumber, direction);
+        }
+    }
+
+    private void sendNotification(String title, String message, int icon, String planeNumber, String direction) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("direction", direction);
+        intent.putExtra("planeNumber", planeNumber);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(icon)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setLights(Color.GREEN, 2000, 2000)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
